6ce032c6f64c580f70f86b9afdd0cdf4cf1680b6
Iñigo Imaña <iimana001@ikasle.ehu.eus>
2023-03-20 14:50:15 +0100

    Refactored classes

---
+++ src/main/java/ehu/eus/chismosas/mastodonfx/presentation/MainController.java
@@ -0,0 +1,45 @@
+package ehu.eus.chismosas.mastodonfx.presentation;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class MainController {
+
+    @FXML
+    private Button bookmarksBtn;
+
+    @FXML
+    private Pane contentPane;
+
+    @FXML
+    private Button exploreBtn;
+
+    @FXML
+    private Button favsBtn;
+
+    @FXML
+    private Button msgBtn;
+
+    @FXML
+    private TextArea newToot;
+
+    @FXML
+    private Button notificationsBtn;
+
+    @FXML
+    private Button profileBtn;
+
+    @FXML
+    private TextField searcher;
+
+    @FXML
+    private Pane titlePane;
+
+    @FXML
+    private ListView<?> tootList;
+
+}
