d36af0a09d6892ffd14378c3267e38ec0bd49e8c
leiregesteira <leiregesteira@gmail.com>
2023-03-22 17:12:13 +0100

    First tootList tryout (rebase)

---
+++ src/main/java/ehu/eus/chismosas/mastodonfx/businesslogic/StatusCell.java
@@ -0,0 +1,55 @@
+package ehu.eus.chismosas.mastodonfx.businesslogic;
+
+import ehu.eus.chismosas.mastodonfx.domain.Status;
+import javafx.scene.control.ListCell;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class StatusCell extends ListCell<Status> {
+    private FXMLLoader loader;
+
+    @FXML
+    private AnchorPane listItem;
+
+    @FXML
+    private Label ability;
+
+    @FXML
+    private Label id;
+
+    @FXML
+    private ImageView picture;
+
+    @Override
+    protected void updateItem(Status item, boolean empty) {
+        super.updateItem(item, empty);
+        if (empty || item == null) {
+            setGraphic(null);
+            setText(null);
+
+            return;
+        }
+
+        if (loader == null) {
+            loader = new FXMLLoader(getClass().getResource("pokemon.fxml"));
+            loader.setController(this);
+            try {
+                loader.load();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        id.setText(item.id);
+       // ability.setText(item.getAbility());
+       // picture.setImage(new Image(item.getPicture()));
+
+        setText(null);
+        setGraphic(listItem);
+    }
+}
