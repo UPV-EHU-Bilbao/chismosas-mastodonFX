575621e7ca4210b8232915f245de4597d0d4c609
heyyder <115657479+heyyder@users.noreply.github.com>
2023-03-23 09:51:00 +0100

    profile-view.fxml corrected and StatusCell class modified

--- src/main/java/eus/ehu/chismosas/mastodonfx/presentation/StatusCell.java
+++ src/main/java/eus/ehu/chismosas/mastodonfx/presentation/StatusCell.java
@@ -1,29 +1,12 @@
 package eus.ehu.chismosas.mastodonfx.presentation;
 
 import javafx.scene.control.ListCell;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import social.bigbone.api.entity.Status;
 
 
 public class StatusCell extends ListCell<Status> {
     private FXMLLoader loader;
-
-    @FXML
-    private AnchorPane listItem;
-
-    @FXML
-    private Label ability;
-
-    @FXML
-    private Label id;
-
-    @FXML
-    private ImageView picture;
 
     @Override
     protected void updateItem(Status item, boolean empty) {
@@ -36,7 +19,7 @@ public class StatusCell extends ListCell<Status> {
         }
 
         if (loader == null) {
-            loader = new FXMLLoader(getClass().getResource("pokemon.fxml"));
+            loader = new FXMLLoader(getClass().getResource("profile-view.fxml"));
             loader.setController(this);
             try {
                 loader.load();
@@ -45,12 +28,7 @@ public class StatusCell extends ListCell<Status> {
             }
         }
 
-
-        id.setText(item.getId());
-       // ability.setText(item.getAbility());
-       // picture.setImage(new Image(item.getPicture()));
-
         setText(null);
-        setGraphic(listItem);
+        //setGraphic(listItem);
     }
 }
