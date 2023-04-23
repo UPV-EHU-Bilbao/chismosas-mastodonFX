d76512712920d72653ec3509107cca2bdb7622f2
heyyder <115657479+heyyder@users.noreply.github.com>
2023-04-23 16:51:30 +0200

    add account button works

--- src/main/java/eus/ehu/chismosas/mastodonfx/presentation/AccountSelection.java
+++ src/main/java/eus/ehu/chismosas/mastodonfx/presentation/AccountSelection.java
@@ -1,18 +1,22 @@
 package eus.ehu.chismosas.mastodonfx.presentation;
 
 import eus.ehu.chismosas.mastodonfx.businesslogic.BusinessLogic;
+import eus.ehu.chismosas.mastodonfx.persistance.DBManager;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 import social.bigbone.api.entity.Account;
+import social.bigbone.api.entity.Application;
 
 import java.io.IOException;
 
-public class AccountSelection {
+public class AccountSelection{
+
 
     @FXML
     private ListView<Account> accountsList;
@@ -21,17 +25,22 @@ public class AccountSelection {
     private Button chooseAccountBtn;
 
     @FXML
-    private Button AddAccountBtn;
+    private Button AddNewAccountBtn;
+
+    @FXML
+    private TextField newID;
+
+    @FXML
+    private TextField newToken;
 
     private Parent root;
 
     @FXML
     private void initialize() {
+        accountsList.setStyle("-fx-control-inner-background: #18181b");
         chooseAccountBtn.disableProperty().bind(accountsList.getSelectionModel().selectedItemProperty().isNull());
-
         accountsList.setCellFactory(param -> new AccountSelectionCell());
         accountsList.getItems().setAll(BusinessLogic.getLoggableAccounts());
-
     }
 
     @FXML
@@ -49,8 +58,29 @@ public class AccountSelection {
         }
     }
 
+    @FXML
+    void addNewAccount() {
+        String id = newID.getText();
+        String token = newToken.getText();
+        try {
+            DBManager.storeAccount(id, token);
+            accountsList.getItems().add(BusinessLogic.getAccount(id));
+            System.out.println("Account added");
+        }catch (Exception e){
+            System.out.println("Couldn't add the account");
+            e.printStackTrace();
+        }
+    }
+
+
     public class AccountSelectionCell extends ListCell<Account> {
         private Parent root;
+
+        @FXML
+        private ImageView avatar;
+
+        @FXML
+        private Label userName;
 
         @Override
         protected void updateItem(Account item, boolean empty) {
@@ -62,7 +92,7 @@ public class AccountSelection {
             }
 
             if (root == null) {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("account.fxml"));
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("account-selection-cell.fxml"));
                 loader.setController(this);
                 try {
                     loader.load();
@@ -79,6 +109,9 @@ public class AccountSelection {
 
             setText(null);
             setGraphic(root);
+
+            userName.setText("@" + item.getUsername());
+            avatar.setImage(ImageCache.get(item.getAvatar()));
 
         }
     }
