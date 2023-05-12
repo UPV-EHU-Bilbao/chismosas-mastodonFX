a263de2abbbc7e34c2c1f89797247e54f9ebf45d
Iñigo Imaña <20203795+inigoim@users.noreply.github.com>
2023-05-12 17:28:27 +0200

    Package into a running jar with maven-shade

---
+++ src/main/java/eus/ehu/chismosas/mastodonfx/presentation/Launcher.java
@@ -0,0 +1,14 @@
+package eus.ehu.chismosas.mastodonfx.presentation;
+
+/**
+ * This class in needed for use as the main class in the jar file. <p>
+ * According to <a href="https://openjfx.io/openjfx-docs/#modular">OpenJFX</a>,
+ * it is the only way to have a runnable file for a non-modular project.
+ * @author Iñigo
+ *
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        MainApplication.main(args);
+    }
+}
